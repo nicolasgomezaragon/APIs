c7b031e33d9deea105916fd5aaea056e9f74a193
Nicolás Gómez Aragón <nicolasgomezaragon@proton.me>
2025-03-14 19:29:34 -0500

    Update Employees.py
    Update data by bringing employee information

--- Employees.py
+++ Employees.py
@@ -12,7 +12,7 @@ c.execute(''' CREATE TABLE IF NOT EXISTS
                 name TEXT, 
                 age INTEGER,
                 position TEXT, 
-                startDate DATE,
+                start_date DATE,
                 sindicate BOOLEAN,
                 salary INTEGER
                 )''')
@@ -32,12 +32,18 @@ def read_employees():
     c.execute("SELECT * FROM employees")
     return c.fetchall()
 
+#######################
+def read_single_employee(employee_id):
+    c.execute("SELECT * FROM employees WHERE employee_id=?",(employee_id))
+    return c.fetchone()
+#####################
+
 
 # <UPDATE> employee
 def update_employee(employee_id, name, age, position, start_date, sindicate, salary):
     c.execute('''
         UPDATE employees 
-        SET name = ?, age = ?, position = ?, startDate = ?, sindicate = ?, salary = ? 
+        SET name = ?, age = ?, position = ?, start_date = ?, sindicate = ?, salary = ? 
         WHERE employee_id = ?''', 
         (name, age, position, start_date, sindicate, salary, employee_id))
     conn.commit()
@@ -61,6 +67,7 @@ def menu():
 
         choice = int(input("Enter your choice: "))
 
+        # CREATE 
         if choice == 1:
             print('Insert employee information')
             employee_id = int(input("ID: ")) 
@@ -71,13 +78,28 @@ def menu():
             sindicate = bool(input("Sindicate(True/False): "))
             salary = int(input("Salary: "))
             add_employee(employee_id, name, age, position, start_date, sindicate, salary)
+        # READ
         elif choice == 2:
             employees = read_employees()
             for emp in employees:
                 print(emp)
+        #UPDATE
         elif choice == 3:
             employee_id = int(input("Enter ID to update data"))
-            # Missing versatile manner to update data
+
+            employee = read_single_employee(employee_id)
+            if employee:
+                print(f"Current data: {employee}")
+                name = input("Name: ") or employee[1]
+                age = input("Age:") or employee[2]
+                position = input("Position:") or employee[3]
+                start_date = input(":") or employee[4]
+                sindicate = input(":") or employee[5] 
+                salary = input(":") or employee[6]
+                update_employee(employee_id, name, age, position, start_date, sindicate, salary)
+            else:
+                print('Employe not found')
+        #DELETE
         elif choice == 4:
             employee_id = int(input("Enter ID to delete employee"))
             delete_employee(employee_id)
