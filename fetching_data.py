caef978378fcc42e68b3bf9665b6400907daa59f
Nick <nicolasgomezaragon@proton.me>
2025-03-18 17:39:47 -0500

    Starting to fetch data

--- fetching_data.py
+++ fetching_data.py
@@ -1,2 +1,16 @@
 # Fetching data from 3rd party API
 # To start cleaning it up
+
+import requests
+
+api_key=mjsbtbAPn5pu53BhqBZp2ocXNW9kT3mn # type: ignore
+
+ticker = "BRK-A"  # Berkshire Hathaway's ticker symbol
+url = f"https://financialmodelingprep.com/api/v3/quote/{ticker}?apikey={api_key}"
+
+response = requests.get(url)
+if response.status_code == 200:
+    data = response.json()
+    print(data)
+else:
+    print(f"Error: {response.status_code}")
