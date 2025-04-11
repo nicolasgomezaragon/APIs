bfa701be8321271381468937a4850bd1f357df16
Nick <nicolasgomezaragon@proton.me>
2025-04-11 09:13:17 -0500

    lol

--- setup.py
+++
@@ -1,8 +0,0 @@
-# filepath: /home/user/Programming/FinancialAPI/setup.py
-from setuptools import setup, find_packages
-
-setup(
-    name="FinancialAPI",
-    version="0.1",
-    packages=find_packages(),
-)
